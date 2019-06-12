df5f139e3932a58e73f1167230e20f89284df6ed
Gustavo T. Ludwig <gustavo.ludwig@gmail.com>
2019-06-12 14:13:25 +0100

    initial front end implementation using thymeleaf

---
+++ src/main/java/ie/gtludwig/citi/simpsons/configuration/AppConfig.java
@@ -0,0 +1,77 @@
+package ie.gtludwig.citi.simpsons.configuration;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    private static final String MESSAGE_SOURCE = "i18n/messages";
+
+
+    @Bean(name = "messageSource")
+	@Description("Spring message resolver")
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename(MESSAGE_SOURCE);
+		messageSource.setFallbackToSystemLocale(false);
+		messageSource.setCacheSeconds(0);
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+    @Bean(name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+        @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        //Important Part
+        templateEngine.addDialect(new LayoutDialect());
+
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+}
