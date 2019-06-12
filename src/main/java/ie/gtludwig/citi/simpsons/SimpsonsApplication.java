d2258ab0dabce15c8e22f05392cb11d49ec9ade2
Gustavo T. Ludwig <gustavo.ludwig@gmail.com>
2019-06-12 09:50:32 +0100

    spring boot initial class

---
+++ src/main/java/ie/gtludwig/citi/simpsons/SimpsonsApplication.java
@@ -0,0 +1,12 @@
+package ie.gtludwig.citi.simpsons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpsonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpsonsApplication.class);
+    }
+}
